cf569da12c9cf7972a257c1ea13bbbca0b4ad052
Issac <aducode@126.com>
2015-10-29 18:33:23 +0800

    add baidu statistics js

---
+++ javascripts/statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?a3bbb0e7f88757e39b20c0b7ebc4836e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
